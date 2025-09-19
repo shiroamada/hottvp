92fb4083fdc74325c502bb0bc9e33dff1cd7e71f
Bernard <bhazama@gmail.com>
2025-09-19 12:26:27 +0800

    put back the fire logo and href to the appurl and change the WOWTV in the footer to href to app url in env too

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -286,7 +286,7 @@
                         <div class="flex flex-col md:flex-row justify-center md:justify-between items-center gap-3 py-5">
                             <div class="flex order-2 md:order-1 gap-2 font-normal text-sm">
                                 <span class="text-muted-foreground">2025©</span>
-                                <a class="text-secondary-foreground hover:text-primary" href="https://wowtv.appollen.my/">WOW TV</a>
+                                <a class="text-secondary-foreground hover:text-primary" href="{{ config('app.url') }}">WOW TV</a>
                             </div>
                             <nav class="flex order-1 md:order-2 gap-4 font-normal text-sm text-secondary-foreground">
                                 <!-- <a class="hover:text-primary" href="https://keenthemes.com/metronic/tailwind/docs">Docs</a>
